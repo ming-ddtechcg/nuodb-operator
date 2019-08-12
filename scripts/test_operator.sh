328ce68bd5f214258b766103c317e011a49bf5ee
Ashutosh Shukla <ashukla@nuodb.com>
2019-08-12 12:15:11 -0400

    Initial Travis build

--- scripts/test_operator.sh
+++ scripts/test_operator.sh
@@ -1,119 +1,79 @@
 #!/bin/bash
-
-helpFunction()
-{
-   echo ""
-   echo "Usage: $0 -u docker-username -p docker-password -e docker-email -s docker-server -r repo-name -t image-tag -c clusterserviceversion"
-   echo -e "\t-u Docker username for pushing the image"
-   echo -e "\t-p Docker password"
-   echo -e "\t-e Docker email"
-   echo -e "\t-s Docker server to push (e.g. quay.io)"
-   echo -e "\t-r Repository name in the server including username"
-   echo -e "\t-t Image tag to be used"
-   echo -e "\t-c clusterserviceversion to use "
-   exit 1 # Exit script after printing help
-}
-
-while getopts "u:p:s:r:t:e:c:h" opt
-do
-   case "$opt" in
-      u ) dusername="$OPTARG" ;;
-      p ) dpassword="$OPTARG" ;;
-      s ) dserver="$OPTARG" ;;
-	  r ) rname="$OPTARG" ;;
-	  t ) tag="$OPTARG" ;;
-	  e ) demail="$OPTARG" ;;
-	  c ) csv="$OPTARG" ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-   esac
-done
-
-# Print helpFunction in case parameters are empty
-if [ -z "$dusername" ] || [ -z "$dpassword" ] || [ -z "$dserver" ] || [ -z "$rname" ] || [ -z "$tag" ] || [ -z "$demail" ] || [ -z "$csv" ]
-then
-   echo "Some or all of the parameters are empty";
-   helpFunction
-fi
-
-
 PROJECT=nuodb
-NODE=centos7.localdomain
-TESTDIR=$HOME/nuodb/nuodb_ocp_test_okd
+NODE=minikube
+TESTDIR=$TRAVIS_BUILD_DIR
 OPERATOR_NAMESPACE=nuodb
 
-yum install -y nc
+kubectl get nodes
 
-if [ ! -d ${TESTDIR} ]; then
-  mkdir -p $TESTDIR
-fi
-cd $TESTDIR
+kubectl label node ${NODE} nuodb.com/node-type=storage
+kubectl label node ${NODE} nuodb.com/zone=nuodb --overwrite=true
 
-# git clone nuodb-ce-operator
-if [ -d ${TESTDIR}/nuodb-ce-operator ]; then
-  rm -fr ${TESTDIR}/nuodb-ce-opeartor
-fi
-cp -r /vagrant/nuodb-ce-operator $TESTDIR/
 
-# Wait for OpenShift API to become ready
-while ! echo exit | nc localhost 8443; do sleep 2; done
 
-sleep 60
+cd $TRAVIS_BUILD_DIR
+echo " This is travis working dir $(pwd)"
 
-#oc login -u system:admin
-
-OCPVERSION=$(oc version | gawk '$1 ~ /^openshift/ { print $NF; }')
-echo $OCPVERSION
-
-docker login -u $dusername -p $dpassword $dserver
-
-oc new-project ${PROJECT}
-
-# Disable THP
-${TESTDIR}/nuodb-ce-operator/nuodb-prereq/thp-tuned.sh
-
-cd ${TESTDIR}
-
-echo "Make local-storage"
-# make local-storage
-sudo mkdir -p /mnt/local-storage/disk0
-sudo chmod -R 777 /mnt/local-storage/
-sudo chcon -R unconfined_u:object_r:svirt_sandbox_file_t:s0 /mnt/local-storage
-sudo chown -R root:root /mnt/local-storage
-
-echo "Label the nodes..."
-oc label node ${NODE} nuodb.com/node-type=storage
-oc label node ${NODE} nuodb.com/zone=east --overwrite=true
-
-echo "Create the storage class and persistent volume..."
-oc create -f ${TESTDIR}/nuodb-ce-operator/deploy/local-disk-class.yaml
-
- oc create secret docker-registry docker-pull-secret \
-    --docker-server=$dserver --docker-username=$dusername \
-    --docker-password=$dpassword --docker-email=$demail
-
-oc secrets link default docker-pull-secret --for=pull
-
-echo "Pulling Docker Image nuodb-ce-operator"
-export NUODB_OP_IMAGE=$dserver/$dusername/$rname:$tag
-docker pull  $NUODB_OP_IMAGE
 
 echo "Create nuodb project..."
-cd ${TESTDIR}/nuodb-ce-operator
+kubectl create namespace $OPERATOR_NAMESPACE
+cd ${TESTDIR}/deploy
 
-echo "Create the K8s Custom Resource Definition for the NuoDB Operator..."
-oc create -f deploy/crds/nuodb_v1alpha1_nuodb_crd.yaml
+export OPERATOR_NAMESPACE=nuodb
+kubectl create -n $OPERATOR_NAMESPACE -f https://raw.githubusercontent.com/k2ieger/testminikube/master/scripts/secret.yaml
+kubectl create -f local-disk-class.yaml
+kubectl create -f cluster_role_binding.yaml
+kubectl create -n $OPERATOR_NAMESPACE -f operatorGroup.yaml
+kubectl create -n $OPERATOR_NAMESPACE -f cluster_role.yaml
+kubectl create -n $OPERATOR_NAMESPACE -f role.yaml
+kubectl create -n $OPERATOR_NAMESPACE -f role_binding.yaml
+kubectl create -n $OPERATOR_NAMESPACE -f service_account.yaml 
+kubectl patch serviceaccount nuodb-operator -p '{"imagePullSecrets": [{"name": "regcred"}]}' -n $OPERATOR_NAMESPACE
+kubectl create -f olm-catalog/nuodb-operator/0.0.5/nuodb.crd.yaml 
+#sed -i 's/"quay.io/ashukla/nuodb-operator:v0.0.5"/"quay.io/nuodb/nuodb-operator-staging:v0.0.5"/g' olm-catalog/nuodb-operator/0.0.5/nuodb.v0.0.5.clusterserviceversion.yaml
+kubectl create  -n $OPERATOR_NAMESPACE -f olm-catalog/nuodb-operator/0.0.5/nuodb.v0.0.5.clusterserviceversion.yaml
 
-echo "Create the K8s Custom Resource Definition for the NuoDB Operator..."
-oc create -f deploy/service_account.yaml
 
-echo "Create the K8s Role Based Access Control for the NuoDB Operator..."
-oc create -n $OPERATOR_NAMESPACE -f deploy/rbac.yaml
+# Check deployment rollout status every 10 seconds (max 10 minutes) until complete.
+ATTEMPTS=0
+ROLLOUT_STATUS_CMD="kubectl rollout status deployment/nuodb-operator -n nuodb"
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+  $ROLLOUT_STATUS_CMD
+  ATTEMPTS=$((attempts + 1))
+  kubectl get pods -n nuodb
+  sleep 10
+done
 
-echo "Create the NuoDB Operator..."
-oc create -n $OPERATOR_NAMESPACE -f deploy/operator.yaml
 
-echo "Create Cluster Service Version for the OLM..."
-oc create -n $OPERATOR_NAMESPACE -f deploy/olm-catalog/nuodb-ce-operator/$csv/nuodb.v$csv.clusterserviceversion.yaml
+kubectl create configmap nuodb-lic-configmap --from-literal=nuodb.lic="" -n $OPERATOR_NAMESPACE
+
 
 echo "Create the Custom Resource to deploy NuoDB..."
-oc create -n $OPERATOR_NAMESPACE -f deploy/cr.yaml
+kubectl create -n $OPERATOR_NAMESPACE -f cr-test.yaml
+
+echo "Sleeping for a while"
+ sleep 20
+
+
+echo "status of all pods"
+kubectl get pods -n nuodb
+
+echo "wait till admin is ready"
+ATTEMPTS=0
+ROLLOUT_STATUS_CMD="kubectl rollout status sts/admin -n nuodb"
+until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+  $ROLLOUT_STATUS_CMD
+  ATTEMPTS=$((attempts + 1))
+  kubectl get pods -n nuodb
+  sleep 10
+done
+
+
+echo "Sleeping for a while"
+ sleep 20
+
+kubectl get pods -n nuodb
+
+kubectl exec -it admin-0 /bin/bash nuocmd show domain -n nuodb
+
+kubectl get sc 
