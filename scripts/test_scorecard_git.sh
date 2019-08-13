8cc6f08b801fc29507f09562ad4d18c224a335a8
Ashutosh Shukla <ashukla@nuodb.com>
2019-08-13 14:47:36 -0400

    Verify operator and scorecard test

---
+++ scripts/test_scorecard_git.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+OP_VER=0.0.5
+OP_PATH=${TRAVIS_BUILD_DIR}/deploy/olm-catalog/nuodb-operator/${OP_VER}/
+DEPLOY_DIR="${TRAVIS_BUILD_DIR}/deploy"
+PKG_NAME=nuodb-operator
+ABS_BUNDLE_PATH="${DEPLOY_DIR}/olm-catalog/${PKG_NAME}/${OP_VER}"
+CR_DIR="${DEPLOY_DIR}/crds"
+NAMESPACE=nuodb
+
+CSV_FILE="$(find "$ABS_BUNDLE_PATH" -name "*${OP_VER}.clusterserviceversion.yaml" -print -quit)"
+CSV_NAME="$(yq r "$CSV_FILE" "metadata.name")"
+NAMESPACE=nuodb
+CR_FILE=$DEPLOY_DIR/cr-test.yaml
+
+echo -e "\n\nRunning operator-sdk scorecard against "$CSV_NAME" with example "$(cat "$CR_FILE" | yq r - "kind")""
+operator-sdk scorecard \
+ --cr-manifest "$CR_FILE" \
+ --crds-dir "$ABS_BUNDLE_PATH" \
+ --olm-deployed \
+ --csv-path "$CSV_FILE" \
+ --namespace "$NAMESPACE" \
+ --init-timeout 60 \
+ --verbose
